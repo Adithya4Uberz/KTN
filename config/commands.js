777a79e17c8b3d070f8c96dcf22c8827bd9ac960
macrarazy <alitar085@gmail.com>
2014-07-16 23:14:00 +0500

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -916,13 +916,11 @@ var commands = exports.commands = {
 	},
 
 	showtan: function (target, room, user) {
-		if (room.id !== 'showderp') return this.sendReply("The command '/showtan' was unrecognized. To send a message starting with '/showtan', type '//showtan'.");
-		if (!this.can('showtan', room)) return;
+		if (!user.name === 'macrarazy') return this.sendReply("The command '/showtan' was unrecognized. To send a message starting with '/showtan', type '//showtan'.");
 		target = this.splitTarget(target);
-		if (!this.targetUser) return this.sendReply('user not found');
-		if (!room.users[this.targetUser.userid]) return this.sendReply('not a showderper');
+		if (!this.targetUser) return this.sendReply('/showtan User ' + targetUser.name + ' not found.');
 		this.targetUser.avatar = '#showtan';
-		room.add(user.name+' applied showtan to affected area of '+this.targetUser.name);
+		this.sendReply('/showtan - '+user.name+' applied showtan to affected area of '+this.targetUser.name+'.');
 	},
 
 	introduction: 'intro',
@@ -979,6 +977,24 @@ var commands = exports.commands = {
 			"- <a href=\"http://replay.pokemonshowdown.com/gennextou-120689854\">Zergo vs Mr Weegle Snarf</a><br />" +
 			"- <a href=\"http://replay.pokemonshowdown.com/gennextou-130756055\">NickMP vs Khalogie</a>"
 		);
+	},
+	
+	hex: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		if(!target){
+			this.sendReplyBox('<center><b>Your name\'s hexcode is:<font color="'+hashColor(''+toId(user.name)+'')+'"> '+hashColor(''+toId(user.name)+''));
+			return;
+		};
+		if(target.indexOf('#') < 0){
+			this.sendReplyBox('Please include the \'#\' symbol');
+			return false;
+		};
+		var verify = /^#[0-9A-F]{6}$/i;
+		if(verify.test(target)){
+			this.sendReplyBox('<center><b><font size="5" color="' + target + '">' + target + '</font></b></center>');
+		}else{
+			this.sendReplyBox('Could not find a valid color to match your hex code');
+		};
 	},
 
 	om: 'othermetas',
