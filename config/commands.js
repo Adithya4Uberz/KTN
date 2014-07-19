71c556741fd2705730a0dbc7867d4df56a56dd43
macrarazy <alitar085@gmail.com>
2014-07-19 05:42:28 +0500

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -374,6 +374,26 @@ var commands = exports.commands = {
 		}
 		return this.parse('/msg ' + this.targetUsername + ', /invite ' + roomid);
 	},
+	
+	/*********************************************************
+	 * Fun commands
+	 *********************************************************/
+
+	poke: function(target, room, user){
+		if(!target) return this.sendReply('/poke needs a target.');
+		return this.parse('/me pokes ' + target + '.');
+	},
+
+	slap: function(target, room, user){
+		if(!target) return this.sendReply('/slap needs a target.');
+		return this.parse('/me slaps ' + target + ' in the face with a slipper!');
+	},
+
+	s: 'spank',
+	spank: function(target, room, user){
+		if(!target) return this.sendReply('/spank needs a target.');
+		return this.parse('/me spanks ' + target + '!');
+	},
 
 	/*********************************************************
 	 * Informational commands
