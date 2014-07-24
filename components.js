ea62447ad3ed4c75166970f4a0e7b210f50493f5
macrarazy <alitar085@gmail.com>
2014-07-24 16:42:25 +0500

    Update components.js

--- components.js
+++ components.js
@@ -226,14 +226,13 @@ var components = exports.components = {
 
                 var targetUser = this.targetUserOrSelf(target);
 
-                if (cmd === 'atm' && !targetUser) {
-                        var money = Core.profile.money(userId);
-                        return this.sendReplyBox('<b><font color="#24678d">' + user.name + ' </font></b>has <b><font color="#24678d">' + Core.profile.display('money', money) + ' </font></b>buck(s).');
-                }
-                if (cmd === 'atm') {
-                        var money = Core.profile.money(targetUser.userid);
-                        return this.sendReplyBox('<b><font color="'#24678d">' + user.name + ' </font></b>has <b><font color="#24678d">' + Core.profile.display('money', money) + ' </font></b>buck(s).');
-                }
+                var money = Core.profile.money(userId);
+
+                if (cmd === 'atm' && !targetUser) return this.sendReplyBox('<b><font color="#24678d">' + user.name + ' </font></b>has <b><font color="#24678d">' + Core.profile.display('money', money) + ' </font></b>buck(s).');
+                
+                var money = Core.profile.money(targetUser.userid);
+
+                if (cmd === 'atm') return this.sendReplyBox('<b><font color="'#24678d">' + user.name + ' </font></b>has <b><font color="#24678d">' + Core.profile.display('money', money) + ' </font></b>buck(s).');
 
                 if (target.length >= 19) return this.sendReply('Usernames are required to be less than 19 characters long.');
 
