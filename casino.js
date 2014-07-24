bd4374b09c1fb51d3ba190e44f920f7273b1c369
macrarazy <alitar085@gmail.com>
2014-07-24 14:13:25 +0500

    Update casino.js

--- casino.js
+++ casino.js
@@ -25,7 +25,7 @@ var dice = {
                 if (!rolls[Users.users[players[1]] === rolls[Users.users[players[0]]]]) {
                         room.addRaw(Users.users[players[0]].name + ' rolled a <font color=red>' + rolls[Users.users[players[0]]] +'</font> ...');
                         room.addRaw(Users.users[players[1]].name + ' rolled a <font color=red>' + rolls[Users.users[players[1]]] + '</font> ...');
-                        room.addRaw('<font color=#24678d> ' + winner + ' wins the dice game and ' +'<font color=red>'+ dice[room.id].bet +'</font> bucks.</font>');
+                        room.addRaw('<font color=#24678d> ' + winner + ' wins the dice game and ' + '<font color=red>'+ dice[room.id].bet + '</font> bucks.</font>');
                         
                         var giveMoney = Number(dice[room.id].bet);
                         var money = Core.stdin('money.csv', Users.users[winner].userid);
@@ -47,9 +47,10 @@ var dice = {
                 var facez = [1,2,3,4,5,6];
                 for (var i=0; i<players.length; i++) {
                         this[room.id].rolls[Users.users[players[i]]] = facez[Math.floor(Math.random()*6)];
-                        }
                 }
-        };
+        }
+
+};
 
 var cmds = {
 
@@ -77,12 +78,23 @@ var cmds = {
         
         joindice: function (target, room, user) {
                 if (!dice[room.id]) return this.sendReply('/joindice - There is no dice currently running in this room.');
+                
+                var giveMoney = Number(dice[room.id].bet);
+                var money = Core.stdin('money.csv', Users.users[winner].userid);
+                var total = Number(money) + Number(giveMoney);
+                Core.stdout('money.csv', Users.users[winner].userid, total);
+                        
+                var takeMoney = Number(dice[room.id].bet);
+                var bucks = Core.stdin('money.csv', Users.users[loser].userid);
+                var amount = Number(bucks) - Number(takeMoney);
+                Core.stdout('money.csv', Users.users[loser].userid, amount);
+                        
                 if (user.money < dice[room.id].bet || isNaN(Number(user.money))) return this.sendReply('/joindice - You can not bet more bucks than you have.');
                 if (dice[room.id].players.indexOf(user.userid) > -1) {
                         this.sendReply('/joindice - You have already joined this dice!');
                         return false;
                 }
-                room.addRaw('<b>'+ user.name + ' has joined the game of dice.</b>');
+                room.addRaw('<b>' + user.name + ' has joined the game of dice.</b>');
                 dice[room.id].players.push(user.userid);
                 if (dice[room.id].players.length === 2) {
                         room.addRaw('<b>The dice game has started!</b>');
@@ -95,7 +107,7 @@ var cmds = {
         enddice: function (target, room, user) {
                 if (!this.canBroadcast()) return;
                 if (!dice[room.id]) return this.sendReply('/enddice - No dice is currently running to end.');
-                room.addRaw('<b>'+ user.name + ' has ended the dice game</b>');
+                room.addRaw('<b>' + user.name + ' has ended the game of dice.</b>');
                 
                 delete dice[room.id];
         }
