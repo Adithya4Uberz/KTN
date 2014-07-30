267d815a06675c3e921d229cb893ddabe2489344
macrarazy <alitar085@gmail.com>
2014-07-31 02:19:26 +0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -697,14 +697,33 @@ var commands = exports.commands = {
 
 	breaklink: 'unlink',
 	unlink: function (target, room, user) {
+		if (!target) return this.parse('/help unlink');
+		if (user.locked || user.mutedRooms[room.id]) return this.sendReply("You cannot do this while unable to talk.");
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser || !targetUser.connected) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
+		if (target.length > MAX_REASON_LENGTH) {
+			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
+		}
+		if (!this.can('unmute', targetUser, room)) return false;
+		
+		this.privateModCommand("" + targetUser.name + "\'s had their links unlinked by " + user.name + "." + (target ? " (" + target + ")" : ""));
+		var alts = targetUser.getAlts();
+		if (alts.get) room.add('|unlink|' + alts);
+		this.add('|unlink|' + this.getLastIdOf(targetUser));
+	},
+	
+	/*breaklink: 'unlink',
+	unlink: function (target, room, user) {
 		if (!this.can('unmute')) return false;
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		var alts = targetUser.getAlts();
 		if (!target) return this.parse('/help unlink');
-		this.send('|unlink|' + this.getLastIdOf(targetUser));
-		this.privateModCommand('(' + targetUser.name + '\'s links have been unlinked.)');
-	},
+		this.addModCommand('(' + targetUser.name + '\'s links have been unlinked.)');
+		this.add('|unlink|' + this.getLastIdOf(targetUser));
+	},*/
 	
 	w: 'warn',
 	warn: function (target, room, user) {
